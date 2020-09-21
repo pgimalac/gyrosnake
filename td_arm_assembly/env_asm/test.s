27e50a6ca1ac2a9891ad554e5035beddb87f9088
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-09-21 15:55:18 +0200

    update test.s

--- td_arm_assembly/env_asm/test.s
+++ td_arm_assembly/env_asm/test.s
@@ -1,3 +1,12 @@
-mov r0, #33
-ldr r0, =33
-ldr r0, =0xC01dCafe
+  mov r0, #33
+  ldr r0, =33
+  ldr r0, =0xC01dCafe
+
+/* initialisation du compteur */
+  mov r0, #10
+loop:
+  subs r0,r0,#1
+  bne loop
+
+end:
+  b end
