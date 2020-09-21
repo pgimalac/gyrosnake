b06f971a586145057899a2175986589d99c41522
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-09-21 23:14:47 +0200

    exo 2 done

--- td_arm_assembly/env_asm/exo2.s
+++ td_arm_assembly/env_asm/exo2.s
@@ -6,18 +6,26 @@
 .global _start
 
 mem_start: .word 0x10000000
+mem_new_start: .word 0x10000300
 pattern: .word 0xdeadbeef
 
 _start:
-
-/* initialisation du compteur */
   mov r0, #64
-  ldr r1,pattern
-  ldr r2,mem_start
-loop:
+  ldr r1, pattern
+  ldr r2, mem_start
+write_pattern:
   subs r0,r0,#1
   str r1,[r2],#4
-  bne loop
+  bne write_pattern
+
+  ldr r2, mem_start
+  ldr r1, mem_new_start
+  mov r0, #64
+copy_mem:
+  subs r0,r0,#1
+  ldr r3, [r2], #4
+  str r3, [r1], #4
+  bne copy_mem
 
 end:
   b end
