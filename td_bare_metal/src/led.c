853fbc0495df67d0f8293a9f2d8b53c0e36b11f7
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-10-09 09:31:54 +0200

    enable gpio port b clock

--- td_bare_metal/src/led.c
+++ td_bare_metal/src/led.c
@@ -1,1 +1,13 @@
 #include "led.h"
+
+void led_init() {
+    // enable GPIO port B clock
+    RCC_AHB2ENR |= 1 << RCC_AHB2ENR_B;
+}
+
+void led_g_on() {
+    //TODO
+}
+void led_g_off() {
+    //TODO
+}
--- td_bare_metal/src/led.h
+++ td_bare_metal/src/led.h
@@ -1,6 +1,17 @@
 #ifndef __LED_H__
 #define __LED_H__
 
+#include <stdint.h>
+
+// RCC register
+#define RCC (*(volatile uint32_t *)0x40021000)
+
+// AHB2 clock enable register, offset=4C -> (RCC + 4C)
+#define RCC_AHB2ENR (*(volatile uint32_t *)0x4002104C)
+
+// port B clock
+#define RCC_AHB2ENR_B 1
+
 void led_init();
 
 void led_g_on();
