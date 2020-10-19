60b730bfaa9c08996d808c9f26eb39f729f714ed
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-10-19 16:00:04 +0200

    uart_init works

--- td_bare_metal/src/uart.c
+++ td_bare_metal/src/uart.c
@@ -1,28 +1,31 @@
 #include "uart.h"
 
 void uart_init() {
+    // disable UART1
+    CLEAR_BIT(USART1->CR1, USART_CR1_UE);
+
     // enable the clock of gpio b
     enable_clock_gpio(RCC_AHB2ENR_GPIOBEN);
 
-    // disable USART1
-    CLEAR_BIT(USART1->CR1, USART_CR1_UE);
-
     // set TX as USART
     GPIOB->MODER = (GPIOB->MODER & ~GPIO_MODER_MODE6_Msk) | GPIO_MODER_MODE6_1;
-    SET_BIT(GPIOB->AFR[0], GPIO_AF7 << GPIO_AFRL_AFSEL6_Pos);
+    SET_BIT(GPIOB->AFR[0],
+            GPIO_AFRL_AFSEL6_0 | GPIO_AFRL_AFSEL6_1 | GPIO_AFRL_AFSEL6_2);
 
     // set RX as USART
     GPIOB->MODER = (GPIOB->MODER & ~GPIO_MODER_MODE7_Msk) | GPIO_MODER_MODE7_1;
-    SET_BIT(GPIOB->AFR[0], GPIO_AF7 << GPIO_AFRL_AFSEL7_Pos);
+    SET_BIT(GPIOB->AFR[0],
+            GPIO_AFRL_AFSEL7_0 | GPIO_AFRL_AFSEL7_1 | GPIO_AFRL_AFSEL7_2);
 
     // enable clock
-    SET_BIT(USART1->CR2, USART_CR2_CLKEN);
+    SET_BIT(RCC->APB2ENR, RCC_APB2ENR_USART1EN);
 
     // specify clock
     CLEAR_BIT(RCC->CCIPR, RCC_CCIPR_USART1SEL);
 
     // reset serial port
     SET_BIT(RCC->APB2RSTR, RCC_APB2RSTR_USART1RST);
+    CLEAR_BIT(RCC->APB2RSTR, RCC_APB2RSTR_USART1RST);
 
     // set serial port speed
     // clock freq = 80MHz, baud = 115200 => USARTDIV = 80000000 / 115200 = 694
@@ -30,17 +33,18 @@ void uart_init() {
     SET_BIT(USART1->BRR, (uint16_t)baud);
 
     // oversampling
-    CLEAR_BIT(USART1->CR1, USART_CR1_OVER8);
-
+    // CLEAR_BIT(USART1->CR1, USART_CR1_OVER8);
     // no parity bit
-    CLEAR_BIT(USART1->CR1, USART_CR1_PCE);
+    // CLEAR_BIT(USART1->CR1, USART_CR1_PCE);
     // 8N1 mode
-    CLEAR_BIT(USART1->CR1, USART_CR1_M);
+    // CLEAR_BIT(USART1->CR1, USART_CR1_M);
     // 1 stop bit
-    CLEAR_BIT(USART1->CR2, USART_CR2_STOP);
+    // CLEAR_BIT(USART1->CR2, USART_CR2_STOP);
 
     // enable USART, sender and receiver
-    SET_BIT(USART1->CR1, USART_CR1_TE | USART_CR1_RE | USART_CR1_UE);
+    // SET_BIT(USART1->CR1, USART_CR1_TE | USART_CR1_RE | USART_CR1_UE);
+    USART1->CR2 = 0;
+    USART1->CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
 }
 
 void uart_putchar(uint8_t c) {
