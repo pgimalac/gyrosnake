160b8fb48ba8e17660050ec603855cd3bc243520
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-10-21 16:54:10 +0200

    write each pin function

--- td_bare_metal/src/matrix.c
+++ td_bare_metal/src/matrix.c
@@ -74,3 +74,94 @@ void matrix_init() {
 
     SET_BIT(GPIOC->BSRR, GPIO_BSRR_BS3);
 }
+
+void SB(int x) {
+    if (x)
+        SET_BIT(GPIOC->BSRR, GPIO_BSRR_BS5);
+    else
+        SET_BIT(GPIOC->BSRR, GPIO_BSRR_BR5);
+}
+
+void LAT(int x) {
+    if (x)
+        SET_BIT(GPIOC->BSRR, GPIO_BSRR_BS4);
+    else
+        SET_BIT(GPIOC->BSRR, GPIO_BSRR_BR4);
+}
+
+void RST(int x) {
+    if (x)
+        SET_BIT(GPIOC->BSRR, GPIO_BSRR_BS3);
+    else
+        SET_BIT(GPIOC->BSRR, GPIO_BSRR_BR3);
+}
+
+void SCK(int x) {
+    if (x)
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS1);
+    else
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR1);
+}
+
+void SDA(int x) {
+    if (x)
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS4);
+    else
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR4);
+}
+
+void C0(int x) {
+    if (x)
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS2);
+    else
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR2);
+}
+
+void C1(int x) {
+    if (x)
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS15);
+    else
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR15);
+}
+
+void C2(int x) {
+    if (x)
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS2);
+    else
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR2);
+}
+
+void C3(int x) {
+    if (x)
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS7);
+    else
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR7);
+}
+
+void C4(int x) {
+    if (x)
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS6);
+    else
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR6);
+}
+
+void C5(int x) {
+    if (x)
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS5);
+    else
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR5);
+}
+
+void C6(int x) {
+    if (x)
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS0);
+    else
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR0);
+}
+
+void C7(int x) {
+    if (x)
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BS3);
+    else
+        SET_BIT(GPIOA->BSRR, GPIO_BSRR_BR3);
+}
--- td_bare_metal/src/matrix.h
+++ td_bare_metal/src/matrix.h
@@ -1,6 +1,34 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include "stm32l4xx.h"
+
 void matrix_init();
+
+void SB(int x);
+
+void LAT(int x);
+
+void RST(int x);
+
+void SCK(int x);
+
+void SDA(int x);
+
+void C0(int x);
+
+void C1(int x);
+
+void C2(int x);
+
+void C3(int x);
+
+void C4(int x);
+
+void C5(int x);
+
+void C6(int x);
+
+void C7(int x);
 
 #endif
