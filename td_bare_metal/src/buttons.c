7017608eafa919387c675fefe0c7db04d469b0df
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-11-01 10:29:28 +0100

    WIP enable interrupt on button

--- td_bare_metal/src/buttons.c
+++ td_bare_metal/src/buttons.c
@@ -1,10 +1,21 @@
 #include "buttons.h"
 #include "stm32l4xx.h"
 
+// button on PC13
+
 void button_init() {
     // enable GPIOC clock
     SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOCEN);
 
     // set pin 13 as output
     CLEAR_BIT(GPIOC->MODER, GPIO_MODER_MODE13_Msk);
+
+    // enable interrupts for PX13
+    SET_BIT(EXTI->IMR1, EXTI_IMR1_IM13);
+
+    // set PC13 as the input source for the interrupt
+    SET_BIT(SYSCFG->EXTICR4, SYSCFG_EXTICR4_EXTI13_PC);
+
+    // interrupt on falling edge
+    SET_BIT(EXTI->FTSR1, EXTI_FTSR1_FT13);
 }
