6ca0a11a2ffaa96afcfc76d6e42b7c4613959243
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-11-06 10:05:57 +0100

    move everything to flash

--- td_bare_metal/src/irq.c
+++ td_bare_metal/src/irq.c
@@ -6,7 +6,7 @@
 extern uint32_t *_stack;
 extern void _start();
 
-void *vector_table[] __attribute__((aligned(512))) = {
+void *vector_table[] __attribute__((aligned(512), section(".vectors"))) = {
     // Stack and Reset Handler
     &_stack, /* Top of stack */
     _start,  /* Reset handler */
