50e670a1cc8100a3efbe4c1f716861e76edb0134
Pierre Gimalac <pierre.gimalac@gmail.com>
2020-11-20 23:12:51 +0100

    WIP gyro

--- src/gyro.c
+++ src/gyro.c
@@ -2,14 +2,32 @@
 
 #include "stm32l475xx.h"
 
-#define I2C_LSM6DSL_ADDR 0b11010100
+#define I2C_LSM6DSL_ADDR 0b1101010
 
 void gyro_init() {
+    // disable i2c2, enable analog filter, disable digital filter
+    CLEAR_BIT(I2C2->CR1, I2C_CR1_PE | I2C_CR1_ANFOFF | I2C_CR1_DNF);
+
     // reset i2c2
-    SET_BIT(RCC->APB1, RCC_APB1_I2C2RST);
+    SET_BIT(RCC->APB1, RCC_APB1RSTR1_I2C2RST);
+
+    // enable clock of GPIO B
+    SET_BIT(RCC->AHB2ENR, RCC_AHB2ENR_GPIOBEN);
+
+    // set PB10 as alternate SCL
+    // set PB11 as alternate SDA
+
+    // if we don't use smbus then not necessary
+    // // set PB12 as alternate SMBA
+
+    // PRESC, SDADEL, SCLDEL, SCLH, SCLL in I2C_TIMINGR ????
+    // NOSTRETCH in I2C_CR1
 
     // enable i2c2 clock
     SET_BIT(RCC->APB1ENR1, RCC_APB1ENR1_I2C2ENR);
+
+    // select PCLK as I2C2 clock
+    CLEAR_BIT(RCC->CCIPR, RCC_CCIPR_USART1SEL);
 
     // use 7 bit addr
     // clear addr bits
@@ -21,5 +39,5 @@ void gyro_init() {
     SET_BIT(I2C2->CR1, I2C_CR1_PE);
 
     // enable i2c2
-    CLEAR_BIT(RCC->APB1, RCC_APB1_I2C2RST);
+    CLEAR_BIT(RCC->APB1, RCC_APB1RSTR1_I2C2RST);
 }
